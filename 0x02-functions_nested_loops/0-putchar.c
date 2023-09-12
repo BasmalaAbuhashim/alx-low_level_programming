90aae262085c5852faa516fe9159e0f49ffba230
“Basmalla <basmalla.m.abohasim@gmail.com>
2023-09-12 11:13:33 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
  * is written by mbah
  * Return: 0
  */
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -14,5 +15,5 @@ int main(void){
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
